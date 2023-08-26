ed885efbb0e540a35673860c53e82e9cabc471bf
minhne432 <nh.minh0403@gmail.com>
2023-08-26 11:52:07 +0700

    first commit

---
+++ import/functionsFile.js
@@ -0,0 +1,20 @@
+//functionsFile.js
+
+//exporting a function
+export function squareNumber(x) {
+    return x * x;
+}
+
+//exporting a variable 
+export const pi = 3.14;
+
+//Cách khác để export:
+
+//exporting a function
+// function squareNumber(x) {
+//     return x * x;
+// }
+
+// //exporting a variable
+// const pi = 3.14;
+// export { squareNumber, pi };
